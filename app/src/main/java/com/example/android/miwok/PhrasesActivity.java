1b04d82fa7666cb95989dbb774d458971053969c
AwesomeJim <jimnahgua@gmail.com>
2018-02-19 15:19:55 +0300

    Update the color,family and phrases activities to use the adapter and same view

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -15,14 +15,52 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
+
+        // Create a list of words
+        ArrayList<Word> Words = new ArrayList<Word>();
+
+        //create a new object word
+
+        Words.add(new Word("Unaenda wapi?","Where are you going?"));
+        Words.add(new Word("Jina lako nani?","What is your name?"));
+        Words.add(new Word("My name is...","My name is..."));
+        Words.add(new Word("Unajiskia vipi?","How are you feeling?"));
+        Words.add(new Word("Mimi nina hisia nzuri.","I’m feeling good."));
+        Words.add(new Word("Je, unakuja?","Are you coming?"));
+        Words.add(new Word("Ndiyo, mimi nina kuj","Yes, I’m coming."));
+        Words.add(new Word("Ninakuja","I’m coming"));
+        Words.add(new Word("Twende","Let’s go"));
+        Words.add(new Word("Njoo hapa.","Come here."));
+
+        //Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        WordAdapter Adapter = new WordAdapter(this,Words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(Adapter);
+
     }
 }
